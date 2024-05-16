89a2badef81dfa8aa109a4f17c0e39fc5d37e82f
Aadish Jain <aadishhere@gmail.com>
2024-05-16 22:16:26 +0530

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Aadish Jain on 16/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
